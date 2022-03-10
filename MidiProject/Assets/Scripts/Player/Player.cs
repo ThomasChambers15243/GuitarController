d77154065a02be4a42b74ea0ac8d930566e31644
TomChambers <tc262389@falmouth.ac.uk>
2022-03-10 10:24:28 +0000

    Added player class

---
+++ MidiProject/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    string name;
+    Dictionary<string, int> scores = new Dictionary<string, int>();
+    
+}
