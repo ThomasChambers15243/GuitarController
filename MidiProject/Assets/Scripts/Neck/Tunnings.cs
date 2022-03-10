041e56bb7421dd6b835b6959bbb2cf78a152e6eb
TomChambers <tc262389@falmouth.ac.uk>
2022-03-10 11:02:40 +0000

    Added dropDTunnings to tunnigns

--- MidiProject/Assets/Scripts/Neck/Tunnings.cs
+++ MidiProject/Assets/Scripts/Neck/Tunnings.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public static class Tunnings
 {
-
     // Notes
+
     public static string[] allOctaveNotes = {"a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"};
 
     // Tunnings from thin string as 0th index to thick string as 5th index
+
     public static string[] standardTunning = { "e", "b", "g", "d", "a", "e" };
+
+    public static string[] dropDTunning = { "d", "b", "g", "d", "a", "e" };
 
 }
