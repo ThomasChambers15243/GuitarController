5996ebc018bc651cbd3dd8c09eab18340501f0e9
TomChambers <tc262389@falmouth.ac.uk>
2022-03-10 10:24:15 +0000

    Added tunning class to hold tunnings and some note data

---
+++ MidiProject/Assets/Scripts/Neck/Tunnings.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Tunnings
+{
+
+    // Notes
+    public static string[] allOctaveNotes = {"a", "a#", "b", "c", "c#", "d", "d#", "e", "f", "f#", "g", "g#"};
+
+    // Tunnings from thin string as 0th index to thick string as 5th index
+    public static string[] standardTunning = { "e", "b", "g", "d", "a", "e" };
+
+}
