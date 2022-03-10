bfec6c7b483dc5a3c448f3cc1736501673ff3710
TomChambers <tc262389@falmouth.ac.uk>
2022-03-10 22:06:58 +0000

    Set up game controller to init the note and strings spawning in the game

--- MidiProject/Assets/Scripts/GameController.cs
+++ MidiProject/Assets/Scripts/GameController.cs
@@ -4,20 +4,64 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+    public GameObject stringSpawner;
+    public GameObject notePrefab;
+    public GameObject stringPrefab;
+    public GameObject noteSpawner;
+
+    // Gaps between strings and notes
+    public float stringGapScalar;
+    public float noteGapScalar;
+
+    
+    private NeckHolder neckHolder;
+    private GameObject[] strings = new GameObject[6];
+    private GameObject[] notes = new GameObject[36];
+
     // Start is called before the first frame update
     void Start()
     {
-        NeckHolder neckHolder = new NeckHolder();
+        // Innits all strings and notes for standard tunnings
+        NeckHolder neckHolder = new NeckHolder(Tunnings.standardTunning);        
+        SpawnStrings(stringSpawner.transform.position);
+        SpawnNotes(noteSpawner.transform.position);
 
+    }
+
+
+
+
+
+
+    /// <summary>
+    /// Spawns strings on the backing
+    /// </summary>
+    void SpawnStrings(Vector3 spawnPosition)
+    {
         for(int i = 0; i < 6; i++)
-        {
-            Debug.Log(neckHolder.strings[i].tunning);
+        {      
+            Quaternion q = new Quaternion(0.707106829f, 0, 0, 0.707106829f);
+            Vector3 spawn = new Vector3(spawnPosition.x + i * stringGapScalar, spawnPosition.y, spawnPosition.z);
+            strings[i] = Instantiate(stringPrefab, spawn , q);
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    /// <summary>
+    /// Spawns note on the strings on the backing
+    /// </summary>
+    void SpawnNotes(Vector3 spawnPosition)
+    {        
+        int noteCounter = 0;
+        for(int i = 0; i < 6; i++)
+        {            
+            for (int j = 0; j < 6; j++)
+            {
+                Quaternion q = new Quaternion(0, 0, 0, 1);
+                Vector3 spawn = new Vector3(spawnPosition.x + i * stringGapScalar, spawnPosition.y, spawnPosition.z + j * noteGapScalar);
+                notes[noteCounter] = Instantiate(notePrefab, spawn, q);
+                noteCounter += 1;
+            }
+        }
     }
+
 }
