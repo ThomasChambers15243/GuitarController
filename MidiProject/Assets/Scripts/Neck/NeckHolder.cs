69d59266ffa52e33dfd4f7640d4bd62b90cf8a0a
TomChambers <tc262389@falmouth.ac.uk>
2022-03-10 14:18:53 +0000

    Added get string methods

--- MidiProject/Assets/Scripts/Neck/NeckHolder.cs
+++ MidiProject/Assets/Scripts/Neck/NeckHolder.cs
@@ -7,18 +7,19 @@ public class NeckHolder : MonoBehaviour
 {
 
     
-    public String[] strings = new String[6];
-    
+    private String[] strings = new String[6];
+
     /// <summary>
     /// Constructor which fills the strings array with string object
     /// </summary>
-    public NeckHolder()
+    /// <param name="guitarTunning">String[] of tunnings for each string, from thin to thick string</param>
+    public NeckHolder(string[] guitarTunning)
     {
         for (int i = 0; i < 6; i++)
         {
             // Settings strings to standard tunnings
             String guitarString = new String();
-            guitarString.tunning = Tunnings.standardTunning[i];
+            guitarString.tunning = guitarTunning[i];
             guitarString.octave = 4;
             guitarString.SetNotes();
             strings[i] = guitarString;            
@@ -26,7 +27,24 @@ public class NeckHolder : MonoBehaviour
     }
 
 
+    /// <returns>Array of String objects</returns>
+    public String[] GetStrings()
+    {
+        return strings;
+    }
 
-
+    /// <param name="stringNumber"></param>
+    /// <returns>String object at the specific number</returns>
+    public String GetStringAtNumber(int stringNumber)
+    {
+        if(stringNumber >= 0 && stringNumber <= 6)
+        {
+            return strings[stringNumber];
+        }
+        else
+        {
+            return null;
+        }
+    }
 
 }
