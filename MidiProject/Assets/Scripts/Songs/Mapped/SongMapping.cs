03f8f85901cc75a7aec96580594e6a583c92da18
TomChambers <tc262389@falmouth.ac.uk>
2022-03-23 17:05:48 +0000

    Added class to map songs from

---
+++ MidiProject/Assets/Scripts/Songs/Mapped/SongMapping.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongMapping 
+{
+    // Individual notes which the user plays
+    public struct MappedNote
+    {
+        public string noteName { get;  set; }
+        public int noteOctave { get; set; }
+        public float duration { get; set; }
+    }
+    // Collection of mapped notes for the entire song
+    private Stack<MappedNote> songMap = new Stack<MappedNote>();
+
+    // Collection of note that have been played, regardless of whether
+    // they were hit or missed
+    private List<MappedNote> notesPlayed = new List<MappedNote>();
+
+    // Total score
+    int score = 0;
+
+    public void AddNoteToMap(MappedNote note)
+    {
+        songMap.Push(note);
+    }
+
+    public void RemoveNoteFromMapAndAddToNotes()
+    {
+        MappedNote nNote = songMap.Pop();
+        notesPlayed.Add(nNote);
+    }
+
+    public void IncremeantScore(bool wasHit)
+    {
+        if (wasHit)
+        {
+            score += 1;
+        }
+    }
+
+    public string NotesPlayed()
+    {
+        string text = "Notes played are: \n";
+        foreach(MappedNote n in notesPlayed)
+        {
+            text += n.noteName;
+            text += "\n";
+        }
+        return text;
+    }
+
+    // Creates and returns a new mapped note
+    public MappedNote MapNote(string _noteName, int _noteOctave, float _duration)
+    {
+        MappedNote mNote = new MappedNote();
+        mNote.noteName = _noteName;
+        mNote.noteOctave = _noteOctave;
+        mNote.duration = _duration;
+        return mNote;
+    }
+
+}
