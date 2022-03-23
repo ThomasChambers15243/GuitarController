c91f4c750c0c32c370539e97419a05832821f912
TomChambers <tc262389@falmouth.ac.uk>
2022-03-23 19:09:20 +0000

    Added first 28 notes of At Doom's Gate

--- MidiProject/Assets/Scripts/Songs/Mapped/MappedSongs/AtDoomsGate.cs
+++ MidiProject/Assets/Scripts/Songs/Mapped/MappedSongs/AtDoomsGate.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class AtDoomsGate
 {
+    // map.MapNote("",,8);
+    // Note reference - https://github.com/robsoncouto/arduino-songs/blob/master/doom/doom.ino
     private SongMapping map;
 
     public void Map()
@@ -13,9 +15,40 @@ public class AtDoomsGate
     
     private void MapNotes()
     {
-        map.MapNote("a#", 2, -2);
-        map.MapNote("",,);
+        // Name...Octave...Duration 
+        // 4 is a quter note 8 is an eighteenth 16 is a sixteenth ext...
+        // Negitive numbers stand for dotted notes, so -4 means a
+        // quarter plus an eighteenth
 
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("d", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("c", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("a#", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("b", 2, 8);
+        map.MapNote("c", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("d", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("c", 3, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("e", 2, 8);
+        map.MapNote("a#", 2, -2);
+        // 28 Notes but thers only 27 (im missing one ahhhhh)^^
     }
     
 
