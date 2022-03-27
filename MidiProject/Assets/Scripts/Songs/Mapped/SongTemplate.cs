a7dc21f6f42b93bb7e44cb1459afc589c8b1a46b
TomChambers <tc262389@falmouth.ac.uk>
2022-03-27 18:27:32 +0100

    Added constructor parameters for time signature values

--- MidiProject/Assets/Scripts/Songs/Mapped/SongTemplate.cs
+++ MidiProject/Assets/Scripts/Songs/Mapped/SongTemplate.cs
@@ -6,14 +6,18 @@ public class SongTemplate
 {
     public string songName;
     public int noteCount;
+    public int timeSigTop;
+    public int timeSigBot;
     public string[] nNames;
     public int[] nOctave;
     public int[] nDur;
 
-    public SongTemplate(string _songName, int _noteCount, string[] _nNames, int[] _nOctave, int[] _nDur)
-    {
+    public SongTemplate(string _songName, int _noteCount, int _timeSigTop, int _timeSigBot, string[] _nNames, int[] _nOctave, int[] _nDur)
+    {        
         songName = _songName;
         noteCount = _noteCount;
+        timeSigTop = _timeSigTop;
+        timeSigBot = _timeSigBot;
         nNames = _nNames;
         nOctave = _nOctave;
         nDur = _nDur;
