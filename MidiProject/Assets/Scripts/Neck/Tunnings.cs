8e6ab4b18358e8d474f5fab623deec370ec72d12
TomChambers <tc262389@falmouth.ac.uk>
2022-04-05 01:29:18 +0100

    Adjusted tunnings

--- MidiProject/Assets/Scripts/Neck/Tunnings.cs
+++ MidiProject/Assets/Scripts/Neck/Tunnings.cs
@@ -16,7 +16,7 @@ public static class Tunnings
 
     // Voltage from the first fret to the 6th fret
     public static float[] voltageFromFret1 = { 4.64f, 4.34f, 3.95f, 3.57f, 3.23f, 2.85f };
-    public static float[] voltageFromBreadBoard = {4.44f,3.88f,3.32f, 2.76f, 2.19f,1.62f };
+    public static float[] voltageFromBreadBoard = {4.43f,3.86f,3.31f, 2.76f, 2.19f,1.62f };
     // Voltage from breaboard voltage ciruit
     public static float[] voltageFromBreadBoardm = { 4.44f, 3.87f, 3.31f, 2.74f, 2.18f, 1.62f };
 
